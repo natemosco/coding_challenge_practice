8bc6339bdd19554344c78a62e5f8fc217ec30116
Nathaniel Mosco <natemosco@gmail.com>
2020-05-06 01:11:03 -0400

    output is doubled need to: figure out why set is not filtering

--- HackerRank_Problems/Three_Sum/three_sum.js
+++ HackerRank_Problems/Three_Sum/three_sum.js
@@ -3,7 +3,8 @@
 
 function threeNumberSum(arr, target) {
     let referenceObj = {};
-    let totalArr = new Set();
+    let totalTriplets = new Set();
+    let tempArr = [];
     // populate reference object with key:value of difference:indexOfActualValue
     // algebra says if a+b+c = total
     // then a+b = total - c
@@ -12,18 +13,26 @@ function threeNumberSum(arr, target) {
         referenceObj[difference] = index;
     }
 
-    for (leti = 0; i < arr.length - 1; i++) {
-        for (letj = i + 1; j < arr.length; j++) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
             let currentTotal = arr[i] + arr[j];
             let compliment_index = referenceObj[currentTotal];
+            if (compliment_index == undefined) {
+                continue;
+            }
             if (compliment_index != i && compliment_index != j) {
-                let tempArr = [arr[i], arr[j], arr[compliment_index]];
+                tempArr = [arr[i], arr[j], arr[compliment_index]];
                 tempArr.sort((a, b) => a - b);
-                totalArr.add(tempArr);
+
+                if (totalTriplets.has(tempArr) == false) {
+                    totalTriplets.add(tempArr);
+                }
             }
         }
     }
-    //    totalArr.sort(a,b)returntotalArr
+    let totalArr = [...totalTriplets];
+    totalArr.sort((a, b) => a - b);
+    return totalArr;
 }
 
 console.log(threeNumberSum([12, 3, 1, 2, -6, 5, -8, 6], 0));
