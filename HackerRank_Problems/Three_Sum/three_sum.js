099025113bb64935dd99318a182b8cb22e6b8d68
Nathaniel Mosco <natemosco@gmail.com>
2020-05-07 11:20:09 -0400

    reflections on three sum

--- HackerRank_Problems/Three_Sum/three_sum.js
+++ HackerRank_Problems/Three_Sum/three_sum.js
@@ -3,6 +3,11 @@
 
 // ! Note two versions of this attempted in JS The first uses an object where the second uses a Set to solve
 
+/* L E A R N I N G   P O I N T S
+ * * Do not try to hash a mutable object, array, or variable.
+ *       when reassigning a variable as a new array UNDER-The-Hood operation is creating an array at a different memory address. This is why you should not hash a mutable structure for reference
+ */
+
 function threeNumberSumA(arr, target) {
     let referenceObj = {};
     let totalArr = [];
@@ -58,7 +63,7 @@ function threeNumberSumA(arr, target) {
 function threeNumberSumB(arr, target) {
     let referenceObj = {};
     let totalTriplets = new Set();
-    let tempArr = [];
+    let totalArr = [];
     // populate reference object with key:value of difference:indexOfActualValue
     // algebra says if a+b+c = total
     // then a+b = total - c
@@ -75,21 +80,38 @@ function threeNumberSumB(arr, target) {
                 continue;
             }
             if (compliment_index != i && compliment_index != j) {
-                tempArr = [arr[i], arr[j], arr[compliment_index]];
+                let tempArr = [arr[i], arr[j], arr[compliment_index]];
                 tempArr.sort((a, b) => a - b);
 
-                if (totalTriplets.has(tempArr) == false) {
-                    totalTriplets.add(tempArr);
+                if (totalTriplets.has(`${tempArr}`) == false) {
+                    totalTriplets.add(`${tempArr}`);
+                    totalArr.push(tempArr);
                 }
             }
         }
     }
-    let totalArr = [...totalTriplets];
-    totalArr.sort((a, b) => a - b);
+
+    totalArr.sort((a, b) => {
+        if (a[0] < b[0]) {
+            return -1;
+        } else if (a[0] > b[0]) {
+            return 1;
+        } else if (a[1] < b[1]) {
+            return -1;
+        } else if (a[1] > b[1]) {
+            return 1;
+        } else if (a[2] < b[2]) {
+            return -1;
+        } else if (a[2] > b[2]) {
+            return 1;
+        } else {
+            return 0;
+        }
+    });
     return totalArr;
 }
 
 // A successful
 console.log(threeNumberSumA([12, 3, 1, 2, -6, 5, -8, 6], 0));
-// B fails to filter using a set but returns the correct answer with 3 multiples of the correct answer
+// B successful
 console.log(threeNumberSumB([12, 3, 1, 2, -6, 5, -8, 6], 0));
